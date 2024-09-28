49ca38bcf9a2a063d3e308c98f28cf4c14c913f4
minorcell <minorcell6789@gmail.com>
2024-09-28 15:54:32 +0800

    release: catch-all router

---
+++ pages/docs/[...params].jsx
@@ -0,0 +1,17 @@
+import { useRouter } from "next/router";
+
+export default function Docs() {
+  const router = useRouter();
+  const { params = [] } = router.query;
+  if (params.length === 2) {
+    return (
+      <h1>
+        You are viewing the [{params[0]} - {params[1]}] page.
+      </h1>
+    );
+  } else if (params.length === 1) {
+    return <h1>You are viewing the [{params[0]}] page.</h1>;
+  }
+
+  return <h1>Doc Home.</h1>;
+}
